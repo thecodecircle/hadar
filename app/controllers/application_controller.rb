2206e37de1776c6421b0656170924f89dcf59fe0
gnpashi <guynatan@gmail.com>
2021-05-15 18:44:30 +0300

    i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  around_action :switch_locale
 
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+  
   def user_index
     if user_signed_in? && current_user.admin?
       @users = User.all.order(id: :asc)
